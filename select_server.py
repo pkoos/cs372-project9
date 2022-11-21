65457abc0b20d142e5a95e13eb89c779131f1ef6
Paul Koos <paully_k5298@yahoo.com>
2022-11-20 20:44:50 -0800

    add logic for the select_server.

--- select_server.py
+++ select_server.py
@@ -4,20 +4,61 @@
 
 import sys
 import socket
-import select
+from select import select
+
+RECV_SIZE = 4096
+HOSTNAME = "localhost"
+
+def socket_data_string(current_socket, data):
+    return f"{current_socket.getpeername()} {len(data)} bytes: {data}"
+
+def disconnect(current_socket, sockets):
+    print(f"{current_socket.getpeername()}: disconnected")
+    sockets.remove(current_socket)
+
+def get_socket_data(current_socket):
+    return current_socket.recv(RECV_SIZE)
+
+def listening_socket_ready(current_socket, server_socket):
+    return current_socket == server_socket
+
+def connection_string(client):
+    return f"{client.getpeername()}: connected"
+
+def create_client_socket(server_socket):
+    client, _ = server_socket.accept()
+    return client
+
+def create_listening_socket(address):
+    server_socket = socket.socket()
+    server_socket.bind(address)
+    server_socket.listen()
+    return server_socket
 
 def run_server(port):
     print("Running the server!")
     print(f"{port}")
-    server_address = ("localhost", port)
+    server_address = (HOSTNAME, port)
 
-    server_socket = socket.socket()
-    server_socket.bind(server_address)
-    server_socket.listen()
+    server_socket = create_listening_socket(server_address)
+
+    sockets = { server_socket }
     while True:
-        client, client_addr = server_socket.accept()
-        print(f"client: {client} client_addr: {client_addr}")
-        break
+        ready_sockets, _, _ = select(sockets, {}, {})
+        for ready_sock in ready_sockets:
+            if listening_socket_ready(ready_sock, server_socket):
+                client = create_client_socket(ready_sock)
+                print(f"{connection_string(client)}")
+                sockets.add(client)
+            else:
+                data = get_socket_data(ready_sock)
+
+                if not data:
+                    disconnect(ready_sock, sockets)
+                else:
+                    print(socket_data_string(ready_sock, data))
+
+
 
 #--------------------------------#
 # Do not modify below this line! #
